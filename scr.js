9b725dac81e9d04628414bd481f79586c729cd86
NkatekoDibakoane <nkatekoz@gmail.com>
2025-05-08 20:14:57 +0200

    DOM rps

--- scr.js
+++ scr.js
@@ -11,36 +11,28 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let hc = prompt("Enter your choice: rock, paper, or scissors");
-    hc = hc.toLowerCase();
-    
-    return hc;
-}
 
+function playRound(humanChoice){
+    let cc=getComputerChoice();
 
-
-
-function playRound(humanChoice, computerChoice){
-
-if (humanChoice === computerChoice) {
+if (humanChoice === cc) {
     console.log("It's a tie!");
     return "tie";
 }
 
 
-else if (humanChoice === "rock" && computerChoice === "scissors") {
+else if (humanChoice === "rock" && cc === "scissors") {
     console.log("You win! Rock crushes scissors.");
     return "human";
 }
 
 
-else if (humanChoice === "scissors" && computerChoice === "paper") {
+else if (humanChoice === "scissors" && cc === "paper") {
     console.log("You win! Scissors cut paper.");
     return "human";
 }
 
-else if (humanChoice === "paper" && computerChoice === "rock") {
+else if (humanChoice === "paper" && cc === "rock") {
     console.log("You win! Paper covers rock.");
     return "human";
 }
@@ -53,31 +45,143 @@ else{
 }
 
 
-humanScore= 0;
-computerScore= 0;
+let humanScore= 0;
+let computerScore= 0;
+round=0;
+let h="";
+let c="";
 
-for (let i=0; i <5 ; i++){
-    let hc = getHumanChoice();
-    let cc = getComputerChoice();
-    w=playRound(hc, cc);
-    if (w === "human"){
+
+
+
+buttonRock= document.querySelector("#rock");
+buttonScissors= document.querySelector("#scissors");
+buttonPaper= document.querySelector("#paper");
+hscore= document.querySelector("#hScore");
+cscore= document.querySelector("#cScore");
+//buttonReset= document.querySelector("#reset");
+
+
+buttonRock.addEventListener("click",()=>{ 
+    
+    let w=playRound("rock");
+    if (w === "tie"){
+        console.log("It's a tie");
+        humanScore++;
+        computerScore++;
+    }
+    else if (w === "human"){
+       console.log("You won the round")
         humanScore++;
     }
-    else if (w==="computer"){
+    else{
+        console.log("You lost the round")
         computerScore++;
     }
-    else if (w==="tie"){
-        humanScore++
+    h= humanScore.toString();
+    c= computerScore.toString();
+    hscore.textContent=h;   
+    cscore.textContent=c;
+    round++;
+    if (round === 5){
+        if (humanScore > computerScore){
+            alert("You won the game!");
+        }
+        else if (humanScore < computerScore){
+            alert("You lost the game!");
+        }
+        else{
+            alert("It's a tie!");
+        }
+        hscore.textContent="0";
+        cscore.textContent="0";
+
+    }
+}
+);
+        
+
+buttonPaper.addEventListener("click", () => {
+    let w=playRound("paper");
+    if(w=== "tie"){
+        console.log("It's a tie");
+        humanScore++;
         computerScore++;
     }
+    else if (w === "human"){
+        console.log("You won the round");
+        humanScore++;
+     }
+    else{
+         console.log("You lost the round");
+            computerScore++;
+     }   
+     h= humanScore.toString();
+     c= computerScore.toString();
+     hscore.textContent=h;
+     cscore.textContent=c;
+    round++;
+    if (round === 5){
+        if (humanScore > computerScore){
+            alert("You won the game!");
+        }
+        else if (humanScore < computerScore){
+            alert("You lost the game!");
+        }
+        else{
+            alert("It's a tie!");
+        }
+        hscore.textContent="0";
+        cscore.textContent="0";
+
+    } ;   
 }
 
-if (humanScore > computerScore){
-    console.log("You won the series");
+);
+
+buttonScissors.addEventListener("click", () =>{
+    let w=playRound("scissors");
+    if(w=== "tie"){
+        console.log("It's a tie");
+        humanScore++;
+        computerScore++;
+    }
+    else if (w === "human"){
+        console.log("You won the round");
+        humanScore++;
+     }
+     else{
+         console.log("You lost the round");
+            computerScore++;
+     }
+     h= humanScore.toString();
+     c= computerScore.toString();
+     hscore.textContent=h;
+     cscore.textContent=c;
+     round++;
+    if (round === 5){
+        if (humanScore > computerScore){
+            alert("You won the game!");
+        }
+        else if (humanScore < computerScore){
+            alert("You lost the game!");
+        }
+        else{
+            alert("It's a tie!");
+        }
+        hscore.textContent="0";
+        cscore.textContent="0";
+
+    }
 }
-else if(humanScore < computerScore){
-    console.log("the computer won the series");
+);
+
+/* /buttonReset.addEventListener("click", () => {
+    humanScore=0;
+    computerScore=0;
+    h= humanScore.toString();
+    c= computerScore.toString();
+    hscore.textContent=h;   
+    cscore.textContent=c;
 }
-console.log(w);
-console.log(humanScore);
-console.log(computerScore);
+);*/
